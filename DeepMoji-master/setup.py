584b88b634816392e1baff00cbd830c26a70d077
xuan zhang <zhangxuan1918@gmail.com>
2021-08-24 15:24:49 +0200

    Update setup.py

--- DeepMoji-master/setup.py
+++ DeepMoji-master/setup.py
@@ -8,9 +8,9 @@ setup(
     include_package_data=True,
     install_requires=[
         'emoji==0.4.5',
-        'h5py==2.7.1',
-        'Keras==2.2.2',
-        'scikit-learn==0.19.1',
+        'h5py>=2.7.1',
+        'Keras>=2.1.2',
+        'scikit-learn>=0.19.1',
         'text-unidecode==1.0',
     ],
 )
